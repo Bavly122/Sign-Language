1a51c220156b0108e6babe0f264e5b60364a037e
Bavly122 <bavlyadel70@gmail.com>
2026-03-10 08:46:46 +0200

    Add files via upload

---
+++ Backend/EnTouch.Application/DTOs/TranslationResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnTouch.Application.DTOs
+{
+    public class TranslationResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string? InputText { get; set; }
+        public string? OutputText { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
